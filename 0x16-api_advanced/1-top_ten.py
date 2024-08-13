7f686435252cd774adf614daf5f082e684b7dfef
D-Murenzi <murenzidavid01@gmail.com>
2024-08-13 23:39:41 +0200

    trying to comply to unknown requrements

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,18 @@ import requests
 
 def top_ten(subreddit):
     """Querry top ten hot subreddits."""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     header = {}
     header['User-Agent'] = 'api-advanced:v1.0.0 (by u/D-Murenzi)'
     parameters = {}
     parameters['limit'] = 10
-    response = requests.get(
-        url, headers=header, allow_redirects=False, params=parameters)
+    response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
+
     results = response.json().get('data').get('children')
+
     a = 0
     while a < 10:
         print(results[a].get('data').get('title'))
