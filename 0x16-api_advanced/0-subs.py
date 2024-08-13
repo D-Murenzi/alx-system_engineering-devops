56acef575a189369d20ebdba3f7ade2c902a8af5
D-Murenzi <murenzidavid01@gmail.com>
2024-08-13 19:22:44 +0200

    advanced api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+"""
+This module contains a function that returns a number of subscribers on
+a given sub-reddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return number of subscribers to the subreddit."""
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {
+        }
+    header['User-Agent'] = 'api-advanced:v1.0.0 (by u/D-Murenzi)'
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    results = response.json().get('data')
+    return results.get('subscribers')
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,4 @@
+This directory contains my project on Api
+It is a little bit advanced for learns like me
+and it is for running purpose.
+Author: D-Murenzi
