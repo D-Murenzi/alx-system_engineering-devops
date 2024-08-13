6c29797ae1adb848a57aff970ca98a48286d4684
D-Murenzi <murenzidavid01@gmail.com>
2024-08-13 21:26:21 +0200

    top ten hot subreddits

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module has function to querry hot subreddits.
+
+the function will then print titles of the top 10 hot subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Querry top ten hot subreddits."""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {}
+    header['User-Agent'] = 'api-advanced:v1.0.0 (by u/D-Murenzi)'
+    parameters = {}
+    parameters['limit'] = 10
+    response = requests.get(
+        url, headers=header, allow_redirects=False, params=parameters)
+    if response.status_code != 200:
+        print(None)
+        return
+    results = response.json().get('data').get('children')
+    a = 0
+    while a < 10:
+        print(results[a].get('data').get('title'))
+        a = a + 1
